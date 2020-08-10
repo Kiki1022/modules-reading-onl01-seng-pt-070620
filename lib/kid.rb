2c92d11d0903dcc2e8e3fcd15438de2321da4037
Kinnari Shah <kinnari1022@yahoo.com>
2020-08-10 01:41:31 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -7,9 +7,9 @@ class Kid
   # extend MetaDancing
    extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
-  attr_accessor :name
+  # attr_accessor :name
  
-  def initialize(name)
-    @name = name
-  end
+  # def initialize(name)
+  #   @name = name
+  # end
 end
